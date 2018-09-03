03bba6f445010db54d4831a0c0055c0b4523c40d
wodeusername <wode@example.com>
2018-09-03 21:28:33 +0800

    first commit

---
+++ fun2/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 21:25:45 2018
+
+@author: liufengming
+"""
+
+def func1():
+    print("This is function 1!")
+    
+def func2():
+    print("你好")
+    
+def func3():
+    print("hello")
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 21:24:43 2018
+
+@author: liufengming
+"""
+
+from distutils.core import setup
+
+setup(name='fun2',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/wodeusername',
+      author='wodeusername',
+      author_email='wode@example.com',
+      license='None',
+      packages=['fun2'],
+      zip_safe=False)
